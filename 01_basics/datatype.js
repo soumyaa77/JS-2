924a835facb0fb6d767d24da6770034f582ebd29
soumyaa77 <109790494+soumyaa77@users.noreply.github.com>
2024-06-24 18:35:16 +0000

    datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+//alert("hello"); //we are using nodejs, not browser
+//code readability should be high
+let name="soumya"
+let age=18
+let isLoggedIn=false
+//number
+//bigInt
+//string -->""
+//boolean =true/false
+//null =standalone value representation of empty value
+//undefined=  value abhi assign hui nhi h
+//symbol = uniqueness k liye use hota
+
+
+//object 
+//null is an object 
+console.log(typeof null); //object
+//typeof undefined= undefined
