9748ff09a8a5bf700bf0edc44e0da2feb996a64b
soumyaa77 <109790494+soumyaa77@users.noreply.github.com>
2024-06-24 19:27:58 +0000

    strings

---
+++ 01_basics/string.js
@@ -0,0 +1,9 @@
+const name="soumya"
+const repoCount=50
+// od way to concat == console.log(name+repoCount+"Value");
+//back ticks `` are the new way to write 
+console.log(`Hello my name is ${name}`);
+//benefit is : string interpolation, we can add methods inside curly braces
+const gameName=new String("hitesh"); //object
+console.log(gameName[0]);
+
